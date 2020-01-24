6089e131d07c67b1195705e3be593dcd9356d777
maheshkumar.p <maheshkumar.p@TTCHP840003.thirdware.com>
2020-01-24 18:08:40 +0530

    Login changes

--- src/main/java/com/testautomation/repositories/LoginRepository.java
+++ src/main/java/com/testautomation/repositories/LoginRepository.java
@@ -1,10 +1,31 @@
 package com.testautomation.repositories;
 
+import java.util.ArrayList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.testautomation.model.Login;
 
-
+@Repository
 public interface LoginRepository extends JpaRepository<Login, String>{
 
+	@Query("select applicationName from Application")
+	public ArrayList<String> getAllApplicationNames();
+
+	@Query("select screenName from Screen where application.applicationID = 1")
+	public ArrayList<String> getAllScreenNames();
+	/*
+	 * @Query(value =
+	 * " SELECT * FROM KVDST02_SHIPMENTPLAN SP WHERE IFNULL(SP.VDST02_APPROVE_F,'N') ='N' ORDER BY SP.VDST02_SHIPMENTPLANNUMBER_C "
+	 * , nativeQuery = true) List<ShipmentPlanDetails> findDistinctShipmentPlanNo();
+	 * 
+	 * @Modifying
+	 * 
+	 * @Query(value =
+	 * " UPDATE KVDST02_SHIPMENTPLAN SP SET SP.VDST02_APPROVE_F = :appreject_flag  WHERE SP.VDST02_SHIPMENTPLANNUMBER_C = :shipmentPlanNo AND IFNULL(SP.VDST02_APPROVE_F,'N') ='N' "
+	 * , nativeQuery = true) void updateApproveFlagByPlanNo(@Param("shipmentPlanNo")
+	 * String shipmentPlanNo, @Param("appreject_flag") String appreject_flag);
+	 */
+	
 }
--- src/main/java/com/testautomation/service/LoginService.java
+++ src/main/java/com/testautomation/service/LoginService.java
@@ -3,12 +3,14 @@ package com.testautomation.service;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.testautomation.model.Application;
 import com.testautomation.model.Login;
 import com.testautomation.repositories.LoginRepository;
 
@@ -31,35 +33,26 @@ public class LoginService {
 	
 	public ArrayList<String> getApplicationNames() {
 		ArrayList<String> applicationList = new ArrayList<String>();
-		applicationList.add("VDS");
-		applicationList.add("ITRS");
-		return applicationList;		
+		/*
+		 * applicationList.add("VDS"); applicationList.add("ITRS");
+		 */
+		ArrayList<String> applicationNameList =	loginrepository.getAllApplicationNames();
+		return applicationNameList;		
 	}
 	
 	public ArrayList<String> getScreenNames(String applicationName) {
-	//public HashMap<String,String> getScreenNames(String applicationName) {
-		/*
-		 * HashMap<String,String> screenNameList = new LinkedHashMap<String,String>();
-		 * if(applicationName.equalsIgnoreCase("VDS")) {
-		 * screenNameList.put("City Master","City Master");
-		 * screenNameList.put("Transfer Route","Transfer Route");
-		 * screenNameList.put("Compound Transfer","Compound Transfer");
-		 * screenNameList.put("Compound Transfer Approval","Compound Transfer Approval"
-		 * ); }else { screenNameList.put("Screen 1","Screen 1");
-		 * screenNameList.put("Screen 2","Screen 2");
-		 * screenNameList.put("Screen 3","Screen 3");
-		 * screenNameList.put("Screen 4","Screen 4"); }
-		 */
 		
 		ArrayList<String> screenNameList = new ArrayList<String>();
-		if(applicationName.equalsIgnoreCase("VDS")) {
-			screenNameList.add("Transfer Route Master");
-			screenNameList.add("Compound Transfer Module");
-		}else {
-			screenNameList.add("ITRS City Master");
-			screenNameList.add("ITRS Dealer Master");
-		}
-		return screenNameList;
+		/*
+		 * if(applicationName.equalsIgnoreCase("VDS")) {
+		 * screenNameList.add("Transfer Route Master");
+		 * screenNameList.add("Compound Transfer Module"); }else {
+		 * screenNameList.add("ITRS City Master");
+		 * screenNameList.add("ITRS Dealer Master"); }
+		 */
+		
+		ArrayList<String> screenNamesList =	loginrepository.getAllScreenNames();		
+		return screenNamesList;
 	}
 	
 
