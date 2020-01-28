8487427372be86ea67539a0b609c07e8fd923b37
maheshkumar.p <maheshkumar.p@TTCHP840003.thirdware.com>
2020-01-28 18:53:16 +0530

    Test Automation Changes

---
+++ src/main/java/com/testautomation/model/TestAutomationModel.java
@@ -0,0 +1,12 @@
+package com.testautomation.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeSet;
+
+public class TestAutomationModel {
+
+	public TreeSet<String> applicationNameList = new TreeSet<String>();	
+	public ArrayList<String> screenDetailsList = new ArrayList<String>(); 		
+	
+}
