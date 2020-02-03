ff247ea41a5c5bba445ef0a694a5706307991f99
Arockiajeganraj A <Arockiajeganraj.A@thirdware.com>
2020-02-03 19:48:14 +0530

    Test suite controller

--- src/main/java/com/testautomation/controller/TestSuiteController.java
+++ src/main/java/com/testautomation/controller/TestSuiteController.java
@@ -1,12 +1,26 @@
 package com.testautomation.controller;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
 import java.nio.file.Paths;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.io.FileUtils;
+import org.apache.poi.util.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.Base64Utils;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,12 +32,12 @@ public class TestSuiteController {
 
 	final static Logger logger = LoggerFactory.getLogger(TestSuiteController.class);
 
+	final static String projectfilePath = Paths.get("").toAbsolutePath().toString();
+
 	@RequestMapping(value = "/uploadTestSuite", method = RequestMethod.POST)
 	public String uploadTestSuite(@RequestParam("file") MultipartFile file,
 			@RequestParam("application") String application, @RequestParam("screen") String screen) {
 		logger.info("Entering @TestSuiteController - ::::");
-		String projectfilePath = Paths.get("").toAbsolutePath().toString();
-
 		String filePath = projectfilePath + File.separator + application + File.separator + screen;
 		try {
 			File saveFile = new File(filePath, "TestSuite_" + application + "_" + screen + ".xlsx");
@@ -36,10 +50,22 @@ public class TestSuiteController {
 		return "success";
 	}
 
-	@RequestMapping(value = "/sampleTestSuite", method = RequestMethod.GET)
-	public String getSampleTestSuite() {
-		logger.info("Entering @TestSuiteController - ::::");
-		return "sf";
+	@RequestMapping(value = "/template", method = RequestMethod.GET)
+	public void downloadTestSuiteTemplate(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		File templateFile = new File(projectfilePath + File.separator + "esip.xlsx");
+		if (templateFile.exists()) {
+			String mimeType = URLConnection.guessContentTypeFromName(templateFile.getName());
+			if (mimeType == null) {
+				mimeType = "application/octet-stream";
+			}
+			response.setContentType(mimeType);
+			response.setHeader("Content-Disposition",
+					String.format("inline; filename=\"" + templateFile.getName() + "\""));
+			response.setContentLength((int) templateFile.length());
+			InputStream inputStream = new BufferedInputStream(new FileInputStream(templateFile));
+			FileCopyUtils.copy(inputStream, response.getOutputStream());
+		}
+
 	}
 
 }
