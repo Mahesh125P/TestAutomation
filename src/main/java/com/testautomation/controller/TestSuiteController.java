3d2a7b90d07c3ab397eed20ab5f44e4a045aed62
Arockiajeganraj A <Arockiajeganraj.A@thirdware.com>
2020-02-04 14:40:39 +0530

    test case method added

--- src/main/java/com/testautomation/controller/TestSuiteController.java
+++ src/main/java/com/testautomation/controller/TestSuiteController.java
@@ -12,14 +12,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.poi.util.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.Base64Utils;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -68,4 +62,20 @@ public class TestSuiteController {
 
 	}
 
+	@RequestMapping(value = "/uploadTestCase", method = RequestMethod.POST)
+	public String uploadTestCase(@RequestParam("file") MultipartFile file,
+			@RequestParam("application") String application, @RequestParam("screen") String screen) {
+		logger.info("Entering @TestSuiteController - ::::");
+		String filePath = projectfilePath + File.separator + "src" + File.separator + "main" + File.separator
+				+ "resources" + File.separator + "TestCase" + application + File.separator;
+		try {
+			File saveFile = new File(filePath, "TestCase_" + application + "_" + screen + ".xlsx");
+			saveFile.mkdir();
+			FileUtils.writeByteArrayToFile(saveFile, file.getBytes());
+		} catch (IllegalStateException | IOException e) {
+			e.printStackTrace();
+			return "exception";
+		}
+		return "success";
+	}
 }
